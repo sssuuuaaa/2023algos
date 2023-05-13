08204adc2d34355b8f2c72d33fe43f7c9f79d336
Yujin Lee <79704758+dldbwls00@users.noreply.github.com>
2023-05-13 23:52:29 +0900

    Create 4354.cpp

---
+++ algos_assignment/이유진_dldbwls00/week2/4354.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string text;
+int table[1000001];
+
+void solve() {
+    cin >> text;
+    while (text[0] != '.') {
+        int sz = text.size();
+        memset(table, 0, sizeof(table));
+        int j = 0;
+        for (int i = 1; i < text.size(); i++) {
+            while (j > 0 && text[i] != text[j]) j = table[j - 1];
+            if (text[i] == text[j]) table[i] = ++j;
+        }
+
+        int div = sz - table[sz - 1];
+        if (sz % div) 
+            cout << 1 << "\n";
+        else 
+            cout << sz / div << "\n";
+
+        cin >> text;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    solve();
+
+    return 0;
+}
