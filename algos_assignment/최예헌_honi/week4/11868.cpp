f348be9dfa9e3c3643d576e03b612e8c131ff71c
honi20 <81567670+honi20@users.noreply.github.com>
2023-05-25 15:44:54 +0900

    Create 11868.cpp

---
+++ algos_assignment/최예헌_honi/week4/11868.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define MAX 10002
+#define INF 987654321
+
+int main(){
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    
+    ll n, p;
+    ll result = 0;
+    
+    cin>>n;
+    
+    while (n--){
+        cin>>p;
+        result ^= p;
+    }
+    
+    if (result == 0)
+        cout<<"cubelover";
+    else
+        cout<<"koosaga";
+    
+    return 0;
+}
