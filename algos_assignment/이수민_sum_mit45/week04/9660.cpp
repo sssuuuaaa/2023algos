d71f7b7dd55c6e8838236ff0c9b61466927535a2
summit45 <81628744+summit45@users.noreply.github.com>
2023-05-26 21:09:20 +0900

    Create 9660.cpp

---
+++ algos_assignment/이수민_sum_mit45/week04/9660.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    long long N;
+    cin >> N;
+    
+    if(N%7==0 || N%7==2) cout << "CY" << "\n";
+    else cout << "SK" << "\n";
+    
+    return 0;
+}
