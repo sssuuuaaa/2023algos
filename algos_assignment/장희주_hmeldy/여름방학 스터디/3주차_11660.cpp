dd9e8163f90831d0a0c160b6f141d89311132f25
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2023-08-21 03:55:34 +0900

    Update 3주차_11660.cpp

--- algos_assignment/장희주_hmeldy/여름방학 스터디/3주차_11660.cpp
+++ algos_assignment/장희주_hmeldy/여름방학 스터디/3주차_11660.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, m, x1, x2, y1, y2;
+    int arr[1025][1025] = { 0 }, pre[1025][1025] = { 0 };
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> arr[i][j];
+            pre[i][j] = arr[i][j] + pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+        }
+    }
+
+    while (m--) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << pre[x2][y2] - pre[x2][y1 - 1] - pre[x1 - 1][y2] + pre[x1 - 1][y1 - 1] << "\n";
+    }
+
+    return 0;
+}
 
