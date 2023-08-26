a9f64ff5a0c849f3a8334880041c0e11a3d94f02
hunt-and-peck <102416954+hunt-and-peck@users.noreply.github.com>
2023-08-26 15:43:46 +0900

    Create 1684.py

---
+++ algos_assignment/원윤서_hap/summer_day4/1684.py
@@ -0,0 +1,14 @@
+import math
+
+n = int(input())
+v = list(map(int, input().split()))
+m = min(v)
+t = [v[k]-m for k in range(n) if v[k]!=m]
+g = t[0]
+
+for k in range(1, len(t)):
+  g = math.gcd(g, t[k])
+  
+print(g)
+
+# 출처: https://sdev.tistory.com/812?category=496802
