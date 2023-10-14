d961b0beea83ecab22d85879d2115d0de3316f55
이예나 <84684759+Yeinalee@users.noreply.github.com>
2023-10-14 11:02:34 -0400

    Update 17387.cpp

--- algos_assignment/이에나_njs07081/fall/week1/17387.cpp
+++ algos_assignment/이에나_njs07081/fall/week1/17387.cpp
@@ -1,57 +1,57 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+
 using namespace std;
 
-#define X first
-#define Y second
-typedef long long ll;
-typedef pair<ll, ll> pll;
+pair<long long, long long> p1;
+pair<long long, long long> p2;
+pair<long long, long long> p3;
+pair<long long, long long> p4;
 
-ll x, y;
-vector<pll> v;
+int ccw(pair<long long, long long> p1, pair<long long, long long> p2,
+        pair<long long, long long> p3) {
+  long long s =
+      p1.first * p2.second + p2.first * p3.second + p3.first * p1.second;
+  s -= (p1.second * p2.first + p2.second * p3.first + p3.second * p1.first);
 
-ll CCW(pll A, pll B, pll C)
-{
-	ll tmp = A.X * B.Y + B.X * C.Y + C.X * A.Y;
-	tmp -= B.X * A.Y + C.X * B.Y + A.X * C.Y;
-
-	if (tmp > 0) return 1; // 반시계
-	else if (tmp == 0) return 0; // 일직선
-	else if (tmp < 0) return -1; // 시계방향
+  if (s > 0)
+    return 1;
+  else if (s == 0)
+    return 0;
+  else
+    return -1;
 }
 
-int solve(pll A, pll B, pll C, pll D)
-{
-	ll ans1 = CCW(A, B, C) * CCW(A, B, D);
-	ll ans2 = CCW(C, D, A) * CCW(C, D, B);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  float a, b;
+  cin >> p1.first >> p1.second;
+  cin >> p2.first >> p2.second;
+  cin >> p3.first >> p3.second;
+  cin >> p4.first >> p4.second;
 
-	if (ans1 == 0 && ans2 == 0) 
-	{
-		// pair 대소비교. 첫번째 인자가 같다면 두번째 인자 비교
-		if (A > B) swap(A, B);
-		if (C > D) swap(C, D);
-		if (A <= D && B >= C) return 1; 
-		else return 0; 
-	}
-	else
-	{
-		if (ans1 <= 0 && ans2 <= 0) return 1; 
-		else return 0;
-	}
-	
-}
+  int p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
+  int p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2);
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+  if (p1p2 == 0 && p3p4 == 0) {
+    if (p1 > p2)
+      swap(p1, p2);
+    if (p3 > p4)
+      swap(p3, p4);
 
-	for (int i = 0; i < 4; i++)
-	{
-		cin >> x >> y;
-		v.push_back({ x,y });
-	}
+    if (p3 <= p2 && p1 <= p4) {
+      cout << 1 << endl;
+      return 0;
+    } else {
+      cout << 0 << endl;
+      return 0;
+    }
+  }
 
-	cout << solve(v[0], v[1], v[2], v[3]);
+  else if (p1p2 <= 0 && p3p4 <= 0)
+    cout << 1 << endl;
+  else
+    cout << 0 << endl;
+  return 0;
 }
